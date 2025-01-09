2a9e7dbffe943948d2a832841254c4c7eb876ae6
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-01-09 13:07:57 +0100

    Fix function names of 1929 and 796

--- 01929-concatenation-of-array/solution.py
+++ 01929-concatenation-of-array/solution.py
@@ -1,5 +1,5 @@
 from typing import List
 
 class Solution:
-    def problem(self, nums: List[int]) -> List[int]:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums + nums
